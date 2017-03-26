5084e5f16a73c3c35d47eba2b896c730fb5640ed
vytlasai <opusone222@gmail.com>
2017-03-26 13:49:51 -0400

    working

---
+++ helloworld/src/main/java/hello/Application.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Vytlasai on 3/26/2017.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
